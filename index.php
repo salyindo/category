51c6c13593349bf2a4338651f8b5f5027c400308
saly <faye02saly@gmail.com>
2026-07-01 01:55:00 +0000

    affichage des categories qui n'ont pas de produits

--- index.php
+++ index.php
@@ -30,7 +30,15 @@ $categories = [
          ]
 ];
 
-
+//2
+function afficheCategorieSansProduit(array $categories): void{
+    foreach ($categories as  $categorie ) {
+        if (empty($categorie["produits"])) {
+            echo $categorie["nom"]."\n";
+        }
+    }
+ }
+ afficheCategorieSansProduit($categories);
 
 
 ?>
