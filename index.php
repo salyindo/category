c397aabc7ae4bef3e862b7f41ef9c2877cd29397
saly <faye02saly@gmail.com>
2026-07-01 01:15:33 +0000

    initialisation du tableau category

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+
+
+$categories = [
+
+   0 =>      [
+            "code" => "coo0",
+            "nom" => "categorie0",
+            "produits" => [
+                  0 => [
+                    "nom" => "produit1",
+                    "reference" => "ref1",
+                    "prix" => 3000,
+                    "quantite" => 5 
+                  ],
+                  1 => [
+                    "nom" => "produit2",
+                    "reference" => "ref2",
+                    "prix" => 2000,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "coo1",
+            "nom" => "categorie1",
+            "produits" => []
+         ]
+];
+
+
+
+
+?>
