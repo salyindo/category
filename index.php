4ea8a1e9a054a70ca7dd92891a09894f271cfbd7
saly <faye02saly@gmail.com>
2026-07-01 01:57:01 +0000

    enristrement d'une nouvelle categorie

--- index.php
+++ index.php
@@ -40,5 +40,53 @@ function afficheCategorieSansProduit(array $categories): void{
  }
  afficheCategorieSansProduit($categories);
 
+//3
+ function saisieChaine(string $message): string {
+     return readline($message);  
+ }
 
+ function champObligatoire(string $value,string $message): bool{
+    if (empty($value)) {
+        echo $message."\n";
+        return  false;
+    }
+        return true;
+ }
+
+ function rechercheCategorieParCle(array $categories, string $key, string $value): int|bool {
+    foreach ($categories as $index  => $categorie ) {
+        if (($categorie[$key]) === $value) {
+            return $index ;
+        }
+    } 
+    return false;
+ }
+
+ function saisieChampObligatoireEtUnique(array $categories,string $smsSaisie, string $smsError,string $key): string{
+        
+    $valueIsValid = true;
+    do {   
+        $value = saisieChaine($smsSaisie);
+        $valueIsValid = champObligatoire($value,$smsError);
+        if($valueIsValid){     
+            $valueIsValid =rechercheCategorieParCle($categories,$key,$value);
+        }
+    } while (!$valueIsValid);
+    return $value;
+ }
+
+ function enregistrerCategorie(): void{
+    global $categories;
+    $code = saisieChampObligatoireEtUnique($categories,"Entrez le code :", "champs obligatoire : ", "code");
+    $nom = saisieChampObligatoireEtUnique($categories,"Entrez le nom :", "champs obligatoire : ", "nom");
+
+    $categorie  =   [
+            "code" => $code,
+            "nom" => $nom,
+            "produits" => []
+         ];
+
+    $categories[] = $categorie;
+ }
+ 
 ?>
